8d852343e603d57b3dc86af9c9994ea22abdb7df
John Kim <john.j.kim@vanderbilt.edu>
2018-05-11 15:31:00 -0400

    added closest-image index plotting for debugging

--- kmeans.m
+++ kmeans.m
@@ -4,70 +4,73 @@ function labels = kmeans(X,test,linfun)
 %   X is (m x n+1) matrix of labeled data
 %   test is (m x n) matrix of unclassified data
 %   linfun is an anonymous linear functional: scalar = linfun(vector)
-    [m,n] = size(X);
+    % [m,n] = size(X);
 
-    % divide X into 5 batches
-    b_size = ceil(m/5);
-    X_batches = mat2cell(X,[b_size*ones(1,4), m-4*b_size],n);
+    % % divide X into 5 batches
+    % b_size = ceil(m/5);
+    % X_batches = mat2cell(X,[b_size*ones(1,4), m-4*b_size],n);
 
-    % interchange validation & training sets, determine accuracy across various k
-    K = [1,2,3];
-    batch_accuracies = zeros(numel(K),5);
+    % % interchange validation & training sets, determine accuracy across various k
+    % K = [1,2,3];
+    % batch_accuracies = zeros(numel(K),5);
 
-    for k=1:numel(K)
-        for b=1:5
-            % create validation set
-            X_val = X_batches{b,1};
+    % for k=1:numel(K)
+    %     for b=1:5
+    %         % create validation set
+    %         X_val = X_batches{b,1};
             
-            % create training set as all batches that are not the validation set
-            % train_batches = find(~(1:5 == b));
-            X_train = vertcat(X_batches{1:5~=b,1});
+    %         % create training set as all batches that are not the validation set
+    %         % train_batches = find(~(1:5 == b));
+    %         X_train = vertcat(X_batches{1:5~=b,1});
 
-            % divide image data and labels
-            y_train = X_train(:,end);
-            X_train = X_train(:,1:end-1);
-            y_val = X_val(:,end);
-            X_val = X_val(:,1:end-1);
-            y_pred = zeros(size(y_val));
+    %         % divide image data and labels
+    %         y_train = X_train(:,end);
+    %         X_train = X_train(:,1:end-1);
+    %         y_val = X_val(:,end);
+    %         X_val = X_val(:,1:end-1);
+    %         y_pred = zeros(size(y_val));
             
-            % predict on validation set
-            [m_val, ~] = size(X_val);
-            for i=1:m_val
-                % calculate distance between validation image and each image in X_train
-                distances = cellfun(linfun, num2cell(X_train-X_val(i,:),2));
+    %         % predict on validation set
+    %         [m_val, ~] = size(X_val);
+    %         for i=1:m_val
+    %             % calculate distance between validation image and each image in X_train
+    %             distances = cellfun(linfun, num2cell(X_train-X_val(i,:),2));
                 
-                % obtain indices of k smallest elements
-                [~,indices] = mink(distances,K(k));
+    %             % obtain indices of k smallest elements
+    %             [~,indices] = mink(distances,K(k));
 
-                % obtain classifications and determine most popular
-                candidates = y_train(indices);
-                candidate_votes = sum(candidates == candidates');
-                [~, idx] = max(candidate_votes);
-                y_pred(i) = candidates(idx);
-            end
+    %             % obtain classifications and determine most popular
+    %             candidates = y_train(indices);
+    %             candidate_votes = sum(candidates == candidates');
+    %             [~, idx] = max(candidate_votes);
+    %             y_pred(i) = candidates(idx);
+    %         end
             
-            batch_accuracies(k,b) = sum(y_pred == y_val) / numel(y_val);
-        end
-    end
+    %         batch_accuracies(k,b) = sum(y_pred == y_val) / numel(y_val);
+    %     end
+    % end
     
-    % plot batch accuracies, compute mean row-wise
-    k_accuracies = mean(batch_accuracies,2);
-    [~, idx] = max(k_accuracies);
-    opt_k = K(idx);
-    fprintf('optimal k: %d\n', opt_k);
+    % % plot batch accuracies, compute mean row-wise
+    % k_accuracies = mean(batch_accuracies,2);
+    % [~, idx] = max(k_accuracies);
+    % opt_k = K(idx);
+    % fprintf('optimal k: %d\n', opt_k);
 
-    acc_vec = batch_accuracies';
-    acc_vec = acc_vec(:);
+    % acc_vec = batch_accuracies';
+    % acc_vec = acc_vec(:);
 
-    figure; scatter(repelem(K,5),acc_vec); title('batch accuracies');
+    % figure; scatter(repelem(K,5),acc_vec); title('batch accuracies');
+
+    opt_k = 1;
 
     % predict on test with opt_k
     % divide training set
     y = X(:,end);
     X = X(:,1:end-1);
-
+    
     [m_test, ~] = size(test);
     labels = zeros(m_test,1);
+    idx_counts = zeros(m_test,1);
     for i=1:m_test
         % calculate distances between test image and each image in X_train
         distances = cellfun(linfun, num2cell(X-test(i,:),2));
@@ -80,5 +83,8 @@ function labels = kmeans(X,test,linfun)
         candidate_votes = sum(candidates == candidates');
         [~, idx] = max(candidate_votes);
         labels(i) = candidates(idx);
+        
+        idx_counts(indices) = idx_counts(indices) + 1;
     end
+    figure; plot(idx_counts);
 end
